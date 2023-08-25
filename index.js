746c0743a0dbb186b076c3b1bf130c280ed1e1a5
mohamdhassan0x7 <1900819@eng.asu.edu.eg>
2023-08-26 02:55:06 +0300

    add Cors

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './DB/connection.js'
 import routers from './src/Modules/index.router.js'
+import cors from'cors'
+
+
+
 
 dotenv.config()
 const app = express()
 
+
+
+
 app.use(express.json())
+app.use(cors({}))
+
 app.use(`${process.env.baseUrl}/auth`, routers.authRouter)
 app.use(`${process.env.baseUrl}/user`, routers.userRouter)
 app.use(`*` , (req,res)=>res.status(404).json({messege:"invalid routng"}))
