0c6e10f156ce8c7442800c51e958258cb9a78123
mohamdhassan0x7 <105478873+mohamdhassan0x7@users.noreply.github.com>
2024-04-26 01:06:27 +0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-    return await mongoose.connect(process.env.DATA_BASE).then(()=>{
+    return await mongoose.connect("mongodb+srv://mohamedhassan225530:BVR9oT78iDcxD6Nd@cluster0.ibnwk9x.mongodb.net/").then(()=>{
         console.log("database connected")
     })
 
